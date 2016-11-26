fa63c6b654ffa8bd440ce6e6d880b48348415342
Benjamin <brskl@msn.com>
2016-11-25 20:04:34 -0800

    Tasks button brings up Contacts activity

--- app/src/main/java/com/example/ben/contacts/MainActivity.java
+++ app/src/main/java/com/example/ben/contacts/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.ben.contacts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickBtnTasks(View view)
+    {
+        Intent explicitIntent = new Intent(this, ContactsActivity.class);
+        startActivity(explicitIntent);
+
     }
 }
