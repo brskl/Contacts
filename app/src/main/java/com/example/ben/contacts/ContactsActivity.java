4e36ec47c17391b33bc35f6ae4017eaac4a667bc
Benjamin <brskl@msn.com>
2016-11-25 20:06:30 -0800

    Removed toolbar and initial populate DB code

--- app/src/main/java/com/example/ben/contacts/ContactsActivity.java
+++ app/src/main/java/com/example/ben/contacts/ContactsActivity.java
@@ -16,24 +16,14 @@ public class ContactsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        //setSupportActionBar(toolbar);
 
         ListView listView = (ListView)findViewById(R.id.listViewTasks);
 
-        // Instead of this, I want you to retrieve the data
-        // from a database (similar to DBSession.
-        List<DBTask> dbTasks = new ArrayList<DBTask>();
-        dbTasks.add(new DBTask("Mike", "Description"));
-        dbTasks.add(new DBTask("Lisa", "Description"));
-        dbTasks.add(new DBTask("Steve", "Description"));
 
-        // HW TODO: Comment code above
-        // DbHelper db = DbHelper.getInstance(this);
-        // db.add("Mike", "Description");
-        // db.add("Lisa", "Simpson's");
-        // db.add("Steve", "The Office");
-        // DBTask[] dbTasks = db.getTasks();
+        DBHelper db = DBHelper.getInstance(this);
+        DBTask[] dbTasks = db.getTasks();
 
         ArrayAdapter<DBTask> arrayAdapter = new ArrayAdapter<DBTask>(
                 this,
